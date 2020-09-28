3733c50ef9dd7929a51b9dc04fcf1250b6239702
Shamindra Sen <akash19.ms97@gmail.com>
2020-09-29 01:22:52 +0530

    Commiting A new package

---
+++ Calculator_Task/src/test_for_git/Test.java
@@ -0,0 +1,10 @@
+package test_for_git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is my First commit using GIT");// TODO Auto-generated method stub
+
+	}
+
+}
